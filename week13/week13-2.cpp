1d930466cac60e095ab70a90a120a5a09be4387a
王學澤 <14750013@me.mcu.edu.tw>
2026-05-20 11:43:35 +0800

    week13

---
+++ week13/week13-2.cpp
@@ -0,0 +1,24 @@
+/// week13-2.cpp
+#include <stdio.h>
+int a = 100, b = 200;/// global 办跑计
+void fun1()
+{
+    int b = 80; /// local 跋办跑计
+    printf("void funl() a is: %d\n", a);
+    printf("void funl() a is: %d\n", b);
+}
+void fun2()
+{
+    b = 30; /// э global 办跑计 跑 30
+}
+int main()
+{
+    int a = 50; /// local 跋办跑计
+    printf("int main()a is: %d\n", a);
+    printf("int main()a is: %d\n", b);
+    fun1();
+    fun2();
+    printf("int main()a is: %d\n", a);
+    printf("int main()a is: %d\n", b);
+}
+
